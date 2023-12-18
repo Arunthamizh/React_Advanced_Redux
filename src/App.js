d5b947f3a9bb1a3eae0dd4a4ff82619fd79cad1a
“Arunthamizh” <arunthamizh.azhagiri@twilightitsolutions.com>
2023-12-18 21:08:40 +0530

    10th-video handling http states and & feedback with redux

--- src/App.js
+++ src/App.js
@@ -1,37 +1,88 @@
-import { useEffect} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { CartAction } from './store/cart-slice';
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { CartAction } from "./store/cart-slice";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
 
-
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCartHook = (state) => {
-    console.log('state',state);
+    console.log("state", state);
 
-  return  state.ui.cartIsVisible
+    return state.ui.cartIsVisible;
   };
   const showCart = useSelector(showCartHook);
-  const cart = useSelector(state => state.cart);
+  const cart = useSelector((state) => state.cart);
+  const notifications = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
 
-    fetch('https://react-cart-eed7d-default-rtdb.firebaseio.com/cart.json',{
-      method: 'PUT',
-      body: JSON.stringify(cart)
-    }).then((response) => {
-      const data =  response.json();
-      console.log('fetch data',data);
+    // ! Below is the example of handling async data in the components using useEffect hook
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+  
+      const response = await fetch(
+        "https://react-cart-eed7d-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+      // const responseData = await sendCartData();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if(isInitial){
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
     });
-  },[cart])
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notifications && (
+        <Notification
+          state={notifications.status}
+          title={notifications.title}
+          message={notifications.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,12 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
     name: "ui",
-    initialState:{ cartIsVisible: false},
+    initialState:{ cartIsVisible: false, notification: null},
     // ! we don`t add the async operation inside the reducers, because reducers are pure functions and async operation free.
     // ! we can add the async operation inside the components(eg: useEffect) or inside the action creators
     reducers: {
         toggle(state){
             state.cartIsVisible = !state.cartIsVisible
+        }, 
+        showNotification(state, action){
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message
+            }
         }
     }
 })
